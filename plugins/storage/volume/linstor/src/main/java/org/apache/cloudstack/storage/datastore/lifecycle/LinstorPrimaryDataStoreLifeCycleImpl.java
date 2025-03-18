b2f420b9fe7a82291177659f34e3730d2e55ab6b
Rene Peinthor <rene.peinthor@linbit.com>
2025-03-18 13:50:19 +0100

    linstor: implement missing deleteDatastore (#10561)
    Somehow deleteDatastore was never implemented, that meant:
    templates haven't been cleaned up on datastore delete and
    also agents have never been informed about storage pool removal.

--- plugins/storage/volume/linstor/src/main/java/org/apache/cloudstack/storage/datastore/lifecycle/LinstorPrimaryDataStoreLifeCycleImpl.java
+++ plugins/storage/volume/linstor/src/main/java/org/apache/cloudstack/storage/datastore/lifecycle/LinstorPrimaryDataStoreLifeCycleImpl.java
@@ -287,7 +287,10 @@ public class LinstorPrimaryDataStoreLifeCycleImpl implements PrimaryDataStoreLif
 
     @Override
     public boolean deleteDataStore(DataStore store) {
-        return dataStoreHelper.deletePrimaryDataStore(store);
+        if (cleanupDatastore(store)) {
+            return dataStoreHelper.deletePrimaryDataStore(store);
+        }
+        return false;
     }
 
     /* (non-Javadoc)
